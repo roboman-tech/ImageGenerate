024ae1520f27ffeaba4a91bbcb6025d059d6fe6b
Roman\Dragon <veselinvujicic08@gmail.com>
2026-02-13 03:46:19 -0500

    Generated test image.

--- generate_image.py
+++ generate_image.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model_path = "D:/models/sdxl-base-1.0"  # folder containing sd_xl_base_1.0.safetensors
+model_path = "D:/Source/models/sdxl-base-1.0"  # folder containing sd_xl_base_1.0.safetensors
 
 # Load the model (VAE is included)
 pipe = StableDiffusionXLPipeline.from_pretrained(
@@ -17,5 +17,5 @@ image = pipe(prompt=prompt, num_inference_steps=70, guidance_scale=7.5).images[0
 plt.imshow(image)
 plt.axis("off")
 plt.show()
-image.save("generated_image_xl.png")
+image.save("D:/Source/ImageGeneration/generated_image_xl.png")
 print("Saved as generated_image_xl.png")
